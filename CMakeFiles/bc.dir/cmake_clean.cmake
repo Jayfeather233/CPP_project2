6ef421de2a9df1f36a47b4f6b10487228c01ea7b
jayfeather233 <jayfeather233@gmail.com>
2022-09-30 18:35:14 +0800

    try

---
+++ CMakeFiles/bc.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/bc.dir/src/main.cpp.o"
+  "CMakeFiles/bc.dir/src/main.cpp.o.d"
+  "bc"
+  "bc.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/bc.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
